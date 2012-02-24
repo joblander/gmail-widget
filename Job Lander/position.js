f204dbce26f9f858732e3838391e8888965e95e6
Brad Phaisan <badd.th@gmail.com>
2012-02-24 00:40:53 -0800

    Add UI for Position detail and the detail expansion. Add Close as a new step. Make widget wider so we have more space.

--- Job Lander/position.js
+++ Job Lander/position.js
@@ -9,30 +9,36 @@ jld.pos.tabButtonsInnerHTML =
 	'<li style="border:0"><a href="#tabs-2" style="padding:0"><img src="//baddth.com/jld/images/plus-icon.png" style="vertical-align:middle"></a></li>';
 
 // innerHTML of a content inside Process Box (To Review, To Apply, ..., etc).
-jld.pos.processBoxInnerHTML =
+jld.pos.stepBoxInnerHTML =
+		'<b style="margin-left:208px">Active Positions</b>'+
 		'<ul style="background:none;border:0">' +
-		'<li class="processBtn ui-state-default round">' +
-			'<a href="#processTab-1">To Review' +
+		'<li class="stepBtn ui-state-default round">' +
+			'<a href="#stepTab-1">To Review' +
 				'<div class="posBoxCount round">10</div>' +
 			'</a>' +
 		'</li>' +
-		'<li class="processBtn ui-state-default">' +
-			'<a href="#processTab-2">To Apply' +
+		'<li class="stepBtn ui-state-default">' +
+			'<a href="#stepTab-2">To Apply' +
 				'<div class="posBoxCount round">0</div>' +
 			'</a>' +
 		'</li>' +
-		'<li class="processBtn ui-state-default">' +
-			'<a href="#processTab-3">Applied and Waiting' +
+		'<li class="stepBtn ui-state-default">' +
+			'<a href="#stepTab-3">Applied and Waiting' +
+				'<div class="posBoxCount round">3</div>' +
+			'</a>' +
+		'</li>' +
+		'<li class="stepBtn ui-state-default">' +
+			'<a href="#stepTab-4">To Schedule' +
 				'<div class="posBoxCount round">0</div>' +
 			'</a>' +
 		'</li>' +
-		'<li class="processBtn ui-state-default">' +
-			'<a href="#processTab-4">To Schedule' +
+		'<li class="stepBtn ui-state-default" style="font-size:10px">' +
+			'<a href="#stepTab-5">Interviewed and Waiting' +
 				'<div class="posBoxCount round">0</div>' +
 			'</a>' +
 		'</li>' +
-		'<li class="processBtn ui-state-default" style="font-size:10px">' +
-			'<a href="#processTab-5">Interviewed and Waiting' +
+		'<li class="stepBtn ui-state-default" style="font-size:10px;margin-left:10px">' +
+			'<a href="#stepTab-6">Closed' +
 				'<div class="posBoxCount round">0</div>' +
 			'</a>' +
 		'</li>' +
@@ -42,36 +48,90 @@ jld.pos.processBoxInnerHTML =
 // innerHTML of a content inside Positions tab.
 // TODO: Use data from server to create this.
 jld.pos.taskBodyInnerHTML =
-		'<h3 style="margin-left:1.4em;margin-top:0">' +
-			'<a href="#" title="Change Search" onclick="return false">' +
-				'Product Manager, San Francisco' +
-				'<div class="round ui-state-default" style="display:inline-block;vertical-align:bottom;cursor:pointer;margin-left:5px">' +
-					'<span class="ui-icon ui-icon-wrench" style="padding:1px"></span>' +
-				'</div>' +
-			'</a>' +
-		'</h3>' +
-		'<div id="processTabs" style="border:0;margin:0;padding:0">' +
-			'<div id="processBox" class="actions">' +
-				jld.pos.processBoxInnerHTML +
+		'<div id="stepTabs" style="border:0;margin:0;padding:0">' +
+			'<div id="stepBox" class="actions">' +
+				jld.pos.stepBoxInnerHTML +
 			'</div>' +
-			'<div id="processTab-1" style="padding:0">' +
+			'<div id="stepTab-1" style="padding:0">' +
+				'<h3 style="text-align:left">' +
+					'<a href="#" title="Change Search" onclick="return false">' +
+						'Product Manager, San Francisco' +
+						'<div class="round ui-state-default" style="display:inline-block;vertical-align:bottom;cursor:pointer;margin-left:5px">' +
+							'<span class="ui-icon ui-icon-wrench" style="padding:1px"></span>' +
+						'</div>' +
+					'</a>' +
+				'</h3>' +
 				'<div class="positions">' +
-					'<div class="pos"><div class="posDg"><span class="ui-icon ui-icon-star"></span></div>Software Engineer at Google</div>' +
-					'<div class="pos"><div class="posDg"><span class="ui-icon ui-icon-star"></span></div>Software Engineer at Facebook</div>' +
-					'<div class="pos"><div class="posDg"><span class="ui-icon ui-icon-star"></span></div>Software Engineer at Apple</div>' +
+					'<div class="pos">' +
+						'<div class="posDg"><span class="ui-icon ui-icon-star"></span></div>' +
+						'<div class="posTitle">Position from feed1</div>' +
+						'<div class="posDetail">Description' +
+						'</div>' +
+					'</div>' +
+					'<div class="pos">' +
+						'<div class="posDg"><span class="ui-icon ui-icon-star"></span></div>' +
+						'<div class="posTitle">Position from feed2</div>' +
+						'<div class="posDetail">Description' +
+						'</div>' +
+					'</div>' +
 				'</div>' +
 			'</div>' +
-			'<div id="processTab-2" style="padding:0">' +
+			'<div id="stepTab-2" style="padding:0">' +
 				'<h3>No Position listed as "To Apply"</h3>' +
 			'</div>' +
-			'<div id="processTab-3" style="padding:0">' +
-				'<h3>No Position listed as "Applied and Wating"</h3>' +
+			'<div id="stepTab-3" style="padding:0">' +
+				'<h3>You have 3 Positions listed as "Applied and Waiting"</h3>' +
+				'<div class="positions">' +
+					'<div class="pos">' +
+						'<div class="posDg"><span class="ui-icon ui-icon-star"></span></div>' +
+						'<div class="posTitle">Product Manager at Google</div>' +
+						'<div class="posDetail">' +
+							'<div class="posDesc">The role: Product Marketing Manager. Consumer... Work closely with Product Managers, Engineers and User Experience teams to develop optimal product for ...</div>' +
+							'<div class="posMail">' +
+								'<div>Related Emails</div>' +
+								'<div>' +
+									'<div class="posIcon"><span class="ui-icon ui-icon-link"></span></div>' +
+									'<div style="display:inline-block"><a href="#inbox">Conformation Email. Thanks for your submission.</a></div>' +
+									'<div class="posDg" style="margin-left:5px;vertical-align:bottom"><span class="ui-icon ui-icon-close"></span></div>' +
+								'</div>' +
+								'<div>' +
+									'<div class="posIcon"><span class="ui-icon ui-icon-link"></span></div>' +
+									'<div style="display:inline-block"><a href="#inbox">Touchbase Email. Hello for recruiter.</a></div>' +
+									'<div class="posDg" style="margin-left:5px;vertical-align:bottom"><span class="ui-icon ui-icon-close"></span></div>' +
+								'</div>' +
+							'</div>' +
+							'<div class="posTask">' +
+								'<div>Related Tasks</div>' +
+								'<div>' +
+									'<div class="posIcon"><span class="ui-icon ui-icon-link"></span></div>' +
+									'<div style="display:inline-block"><a href="#inbox">Prepare an interview with recruiter</a></div>' +
+									'<div class="posDg" style="margin-left:5px;vertical-align:bottom"><span class="ui-icon ui-icon-close"></span></div>' +
+								'</div>' +
+							'</div>' +
+						'</div>' +
+					'</div>' +
+					'<div class="pos">' +
+						'<div class="posDg"><span class="ui-icon ui-icon-star"></span></div>' +
+						'<div class="posTitle">Product Manager at Facebook</div>' +
+						'<div class="posDetail">Description' +
+						'</div>' +
+					'</div>' +
+					'<div class="pos">' +
+						'<div class="posDg"><span class="ui-icon ui-icon-star"></span></div>' +
+						'<div class="posTitle">Product Manager at Apple</div>' +
+						'<div class="posDetail">Description' +
+						'</div>' +
+					'</div>' +
+				'</div>' +
 			'</div>' +
-			'<div id="processTab-4" style="padding:0">' +
+			'<div id="stepTab-4" style="padding:0">' +
 				'<h3>No Position listed as "To Schedule"</h3>' +
 			'</div>' +
-			'<div id="processTab-5" style="padding:0">' +
+			'<div id="stepTab-5" style="padding:0">' +
 				'<h3>No Position listed as "Interviewed and Waiting"</h3>' +
+			'</div>' +
+			'<div id="stepTab-6" style="padding:0">' +
+				'<h3>Under Construction</h3>' +
 			'</div>' +
 		'</div>';
 
@@ -95,13 +155,23 @@ jld.pos.init = function() {
 
 // A function to run after all elements are put into the widget.
 jld.pos.render = function() {
-	$('#processTabs').tabs({
-		selected: 0,
-		collapsible: false,
+	$('#stepTabs').tabs({
+		selected: -1,
+		collapsible: true,
 		fx: [{ opacity: 'toggle', duration: 'fast' },{ height: 'toggle', duration: 'fast' }]
 	});
 	$("#newPosBtn,.posBox").button();
 	$("#newPosBtn").click(jld.pos.createNewPosition);
+	$(".pos").click(function(){
+		if (!$(this).hasClass("posExpand")){
+			$(this).addClass("posExpand");
+		} else {
+			$(this).removeClass("posExpand");
+		}
+	});
+	$(".posDetail").click(function(){
+		event.stopPropagation();
+	});
 
 	// TODO(baddth): Check with Trenton about this.
     //TODO: Make .lander_datepicker class elements pop out a datepicker. Not sure why the following code doesn't work:
